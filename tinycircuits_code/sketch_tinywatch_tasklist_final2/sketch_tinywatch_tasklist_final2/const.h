916ac81c450bd7a46dd79b7e81536c8efcff3243
Julian Poon <nailuj.noop@gmail.com>
2019-11-19 15:23:51 +0800

    Added EEPROM to guan tsin's code ^-^

---
+++ tinycircuits_code/sketch_tinywatch_tasklist_final2/sketch_tinywatch_tasklist_final2/const.h
@@ -0,0 +1,8 @@
+// ################################
+// ####### GLOBAL CONSTANTS #######
+// ################################
+
+#define N_TODO 20     // max number of to do to store
+#define LEN_TODO 31   // max length of each todo (not inclusive of the \0 char)
+#define Rx 96         // Screen resolution x
+#define Ry 64         // Screen resolution y
